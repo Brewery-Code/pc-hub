025085649ead75aa73b783d8ddb4d0753ef4ecdd
Oleksandr Yakymchuk <oleksandr_yakymchuk@protonmail.com>
2024-12-20 15:32:06 +0200

    Змінено опис swagger

--- backend/mysite/urls.py
+++ backend/mysite/urls.py
@@ -11,7 +11,7 @@ schema_view = get_schema_view(
     openapi.Info(
         title="Pc-hub API",
         default_version="v1",
-        description="Поки не придумав",
+        description="Опис апі",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="contact@snippets.local"),
         license=openapi.License(name="BSD License"),
