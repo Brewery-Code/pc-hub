8a8f1c2f5e0f8e2289eae112b2838854782f86f0
Vitalii <x4gm9k@gmail.com>
2024-12-27 00:44:00 +0200

    Footer was added to the site

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,111 @@
+import { ReactComponent as ViberSmallIcon } from '../../assets/icons/viber-small.svg';
+import { ReactComponent as TelegramSmallIcon } from '../../assets/icons/telegram-small.svg';
+import { ReactComponent as InstagramIcon } from '../../assets/icons/instagram.svg';
+import { ReactComponent as FacebookIcon } from '../../assets/icons/facebook.svg';
+import { ReactComponent as ViberIcon } from '../../assets/icons/viber.svg';
+import { ReactComponent as TelegramIcon } from '../../assets/icons/telegram.svg';
+import { ReactComponent as YoutubeIcon } from '../../assets/icons/youtube.svg';
+import { ReactComponent as LinkedInIcon } from '../../assets/icons/linkedin.svg';
+import { ReactComponent as SendEmailButtonIcon } from '../../assets/icons/send-button.svg';
+import { ReactComponent as LogoIcon } from '../../assets/icons/logo.svg';
+import { ReactComponent as MastercardIcon } from '../../assets/icons/mastercard.svg';
+import { ReactComponent as VisaIcon } from '../../assets/icons/visa.svg';
+
 import styles from './Footer.module.css';
 
 export default function () {
+  const information = ['Акції', 'Кредит', 'Оплата і доставка', 'Гарантія', 'Часті запитання', 'Новини', 'Блог', 'Про нас', 'Політика конфіденційності', 'Контакти'];
+  const services = ['Сервісний центр v-comp', 'Магазин Б/У товарів', 'Скупка Б/У', 'Ремонт ПК і оргтехніки', "Комп'ютерна допомога", 'Співробітництво', 'Головна'];
+
   return (
-    <div className="">Footer</div>
+    <footer className={styles.footer}>
+      <div className="footer__container">
+        <div className={styles.footer__body}>
+          <div className={styles.block}>
+            <h6 className={styles.block__title}>Інформація</h6>
+            <ul className={styles.block__list}>
+              {information?.map(item => {
+                return <li key={item} className={styles.block__item}><a href="">{item}</a></li>;
+              })}
+            </ul>
+          </div>
+          <div className={styles.block}>
+            <h6 className={styles.block__title}>Послуги та сервіси</h6>
+            <ul className={styles.block__list}>
+              {services?.map(item => {
+                return <li key={item} className={styles.block__item}><a href="">{item}</a></li>;
+              })}
+            </ul>
+          </div>
+          <div className={styles.block}>
+            <h6 className={styles.block__title}>Контакти</h6>
+            <div className={styles.block__list}>
+              <ul className={styles.block}>
+                <li className={styles.block__item}>(067) 11-12-485 - Отдел продаж</li>
+                <li className={styles.block__item}>(066) 484-39-22 - Отдел продаж</li>
+                <li className={styles.block__item}>
+                  (063) 747-33-48 - Отдел продаж
+                  <ViberSmallIcon />
+                  <TelegramSmallIcon />
+                </li>
+              </ul>
+              <div className={`${styles.block__item} ${styles.location}`}>
+                Дніпро<br />
+                Європейська, 8 (минула Миронова 8)
+              </div>
+              <ul className={`${styles.block__item} ${styles.schedule}`}>
+                Понеділок-п'ятниця 9:00-19:00<br />
+                Субота-Неділя: з 9:00-16:00
+              </ul>
+            </div>
+          </div>
+          <div className={styles.block}>
+            <h6 className={styles.block__title}>Слідкуйте за нами</h6>
+            <ul className={styles.social}>
+              <li className={styles.social__item}><InstagramIcon /></li>
+              <li className={styles.social__item}><FacebookIcon /></li>
+              <li className={styles.social__item}><ViberIcon /></li>
+              <li className={styles.social__item}><TelegramIcon /></li>
+              <li className={styles.social__item}><YoutubeIcon /></li>
+              <li className={styles.social__item}><LinkedInIcon /></li>
+            </ul>
+            <div className={styles.subscription}>
+              <div className={styles.subscription__title}>Підписуйтесь на знижки</div>
+              <form className={styles.subscription__form}>
+                <input
+                  type="text"
+                  placeholder='Вкажіть ваш email...'
+                />
+                <button>
+                  <SendEmailButtonIcon width='24px' height='24px' />
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className={styles.licenses}>
+            <div className={styles.licenses__logo}><LogoIcon /></div>
+            <div className={`${styles.subscription} ${styles['only-mobile']}`}>
+              <div className={styles.subscription__title}>Підписуйтесь на знижки</div>
+              <form className={styles.subscription__form}>
+                <input
+                  type="text"
+                  placeholder='Вкажіть ваш email...'
+                />
+                <button>
+                  <SendEmailButtonIcon width='24px' height='24px' />
+                </button>
+              </form>
+            </div>
+            <div className={styles.licenses__text}>
+              2008-2021 Интернет-магазин v-comp.com.ua Все права защищены
+            </div>
+            <div className={styles.licenses__payment}>
+              <div><MastercardIcon /></div>
+              <div><VisaIcon /></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 }
