c85f7bff0f27f9eec5349fa7bf0d97092a0e8e00
Vitalii <x4gm9k@gmail.com>
2025-03-11 23:31:36 +0200

    Product page was updated

--- frontend/src/pages/Product/Body/Body.tsx
+++ frontend/src/pages/Product/Body/Body.tsx
@@ -3,6 +3,12 @@ import styles from "./Body.module.css";
 import { useTranslation } from "react-i18next";
 import { IProduct } from "../../../store/product/product.slice";
 import { useEffect, useState } from "react";
+import {
+  ArrowBold,
+  ArrowCommon,
+  ComparisonIcon,
+  LikeIcon,
+} from "../../../assets/icons";
 
 interface IBodyProps {
   product: IProduct;
@@ -25,14 +31,16 @@ function Body({ className, product }: IBodyProps) {
     <div className={clsx(className, styles.body)}>
       <div className={styles.left}>
         <div className={styles.images}>
-          <div className={styles.images__list}>
-            {product.images.map((image) => (
-              <img
-                src={image.image}
-                alt="productImg"
-                onClick={() => changeActiveImg(image.image)}
-              />
-            ))}
+          <div className={styles.images__container}>
+            <div className={styles.images__list}>
+              {product.images.map((image) => (
+                <img
+                  src={image.image}
+                  alt="productImg"
+                  onClick={() => changeActiveImg(image.image)}
+                />
+              ))}
+            </div>
           </div>
           <div className={styles.image__main}>
             <img src={activeImg} alt="productImg" />
@@ -50,9 +58,36 @@ function Body({ className, product }: IBodyProps) {
               </li>
             ))}
           </ul>
+          <button className={styles["characteristics__show-all"]}>
+            {t("characteristics.showAll")}
+            <ArrowCommon
+              className={styles["characteristics__show-all-arrow"]}
+            />
+          </button>
+          <div className={styles.description}>
+            <h4 className={styles.description__title}>
+              {t("description.title")}
+            </h4>
+            <p className={styles.description__text}>{product.description}</p>
+            <button className={styles.description__more}>
+              {t("description.showAll")}
+              <ArrowBold className={styles["description__more-arrow"]} />
+            </button>
+          </div>
         </div>
       </div>
-      <div className={styles.right}></div>
+      <div className={styles.right}>
+        <div className={styles.head}>
+          <div className={styles.head__new}>{t("banners.isNew")}</div>
+          <div className={styles.head__hit}>{t("banners.topSales")}</div>
+          <div className={styles.head__comparison}>
+            <ComparisonIcon className={styles["head__comparison-icon"]} />
+          </div>
+          <div className={styles.head__like}>
+            <LikeIcon className={styles["head__like-icon"]} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
