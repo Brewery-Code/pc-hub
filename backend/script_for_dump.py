24d1ac145c01905efd31eef9f6bde56f27e35b2b
mrsy-21 <oleksandr_yakymchuk@protonmail.com>
2025-04-15 19:32:06 +0300

    Add file data.json. backup.json file was removed

---
+++ backend/script_for_dump.py
@@ -0,0 +1,12 @@
+# script_for_dump.py
+import os
+import django
+from django.core.management import call_command
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+django.setup()
+
+with open("data.json", "w", encoding="utf-8") as f:
+    call_command(
+        "dumpdata", "core", "news", "orders", "product", "user", indent=4, stdout=f
+    )
