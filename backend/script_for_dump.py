8923660b84854f1d619bdc89328819d4ab6f2b65
mrsy-21 <oleksandr_yakymchuk@protonmail.com>
2025-04-18 10:52:19 +0300

    Update backup.json file

--- backend/script_for_dump.py
+++ backend/script_for_dump.py
@@ -6,7 +6,7 @@ from django.core.management import call_command
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 django.setup()
 
-with open("data.json", "w", encoding="utf-8") as f:
+with open("backup.json", "w", encoding="utf-8") as f:
     call_command(
         "dumpdata", "core", "news", "orders", "product", "user", indent=4, stdout=f
     )
