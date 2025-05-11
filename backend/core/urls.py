81c75962cfb8bca4904c094844477ba1c695199f
mrsy-21 <oleksandr_yakymchuk@protonmail.com>
2025-05-11 17:47:06 +0300

    Change urls names

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -7,7 +7,7 @@ router.register(r"wishlist", views.WishlistViewSet, basename="wishlist")
 
 urlpatterns = [
     path("partners/", views.PartnerApiView.as_view(), name="partners"),
-    path("banners/", views.BannerView.as_view()),
+    path("banners/", views.BannerView.as_view(), name="banners"),
     path("reviews/", views.ReviewAPIView.as_view(), name="reviews"),
     path("", include(router.urls)),
 ]
--- backend/product/urls.py
+++ backend/product/urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("categories/", views.TopLevelCategoryView.as_view(), name="category-list"),
+    path("categories/", views.TopLevelCategoryView.as_view(), name="category_list"),
     path(
         "categories/<int:parent_id>/children/",
         views.CategoryView.as_view(),
-        name="category-children",
+        name="category_children",
     ),
-    path("products/", views.ProductListView.as_view(), name="all products"),
+    path("products/", views.ProductListView.as_view(), name="all_products"),
     path(
-        "products/<str:identifier>/", views.ProductDetailView.as_view(), name="Product"
+        "products/<str:identifier>/", views.ProductDetailView.as_view(), name="product"
     ),
     path(
         "<int:category_id>/brands/",
